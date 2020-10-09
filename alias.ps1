46bc0d8bad0a6392d660028d605ec39f34182730
Maksim Makushchenko <maksalmak@gmail.com>
2020-10-09 18:48:19 +0300

    Add prod file for swarm

--- alias.ps1
+++ alias.ps1
@@ -1,8 +1,8 @@
 function LandingDockerCompose {
-    docker-compose -f .\docker-compose.yml -f .\docker-compose.override.yml -f .\Landing-API\docker-compose.yml -f .\Landing-API\docker-compose.override.yml -f .\Landing-Front\docker-compose.yml -f .\Landing-Front\docker-compose.override.yml $args
+    docker-compose -f .\Landing-API\docker-compose.yml -f .\Landing-API\docker-compose.override.yml -f .\Landing-Front\docker-compose.yml -f .\Landing-Front\docker-compose.override.yml -f .\docker-compose.yml -f .\docker-compose.override.yml $args
 }
 function LandingDockerComposeStack {
-    docker-compose -f .\docker-compose.yml -f .\Landing-API\docker-compose.yml -f .\Landing-Front\docker-compose.yml $args
+    docker-compose -f .\Landing-API\docker-compose.yml -f .\Landing-Front\docker-compose.yml -f .\docker-compose.yml -f .\docker-compose.prod.yml $args
 }
 Set-Alias -Name ldc LandingDockerCompose
 Set-Alias -Name ldcs LandingDockerComposeStack
