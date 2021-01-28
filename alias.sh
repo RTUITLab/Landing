b30882b1bd5c5b772c37c6b4791b42c8bcfc25a2
Maksim Makushchenko <maksalmak@gmail.com>
2021-01-28 06:31:24 +0300

    Update api

--- alias.sh
+++ alias.sh
@@ -1,2 +1,2 @@
 alias ldc='ocker-compose -f .\Landing-API\docker-compose.yml -f .\Landing-API\docker-compose.override.yml -f .\Landing-Front\docker-compose.yml -f .\Landing-Front\docker-compose.override.yml -f .\docker-compose.yml -f .\docker-compose.override.yml $args'
-alias ldcs='docker-compose -f .\Landing-API\docker-compose.yml -f .\Landing-Front\docker-compose.yml -f .\docker-compose.yml -f .\docker-compose.prod.yml $args'
+alias ldcs='docker-compose -f .\Landing-API\docker-compose.yml -f .\Landing-API\docker-compose.prod.yml -f .\Landing-Front\docker-compose.yml -f .\docker-compose.yml -f .\docker-compose.prod.yml $args'
